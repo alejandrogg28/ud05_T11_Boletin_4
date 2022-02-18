d13918aeba0320535095a75e85b2b3951b4a4568
Alejandro Garcia <alejandrogg28@gmail.com>
2022-02-18 09:11:18 +0100

    Ejercicio 1 Boletín 4 primer commit

---
+++ exercicio_1.html.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 1 Boletín 4</title>
+</head>
+<body>
+<!--Muestra los números múltiplos de 5 de 0 a 100 utilizando un bucle for.-->    
+
+
+    <p>Los múltiplos de 5 son: </p>
+
+
+
+    <?php
+        for ($i=0; $i < 100; $i++) { 
+            # code...
+        }
+    
+    ?>
+
+</body>
+</html>
+
