998175cfce705ca20c96e1c289aa73dffc8b01ca
Alejandro Garcia <alejandrogg28@gmail.com>
2022-02-21 10:31:08 +0100

    Ejercicio 7 Boletín 4 tercer commit

--- exercicio_7.html.php
+++ exercicio_7.html.php
@@ -4,34 +4,41 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ejercicio 7 Boletín 4</title>
+    <title>ejercicio7</title>
 </head>
 <body>
-<!--Realiza el control de acceso a una caja fuerte. La combinación será un número de 4 cifras. El
-programa nos pedirá la combinación para abrirla. Si no acertamos, se nos mostrará el mensaje
-“Lo siento, esa no es la combinación” y si acertamos se nos dirá “La caja fuerte se ha abierto
-satisfactoriamente”. Tendremos cuatro oportunidades para abrir la caja fuerte-->    
-
-    <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
-        <label for="combinacion">Combinación</label>
-        <input type="number" name="combinacion"/><br/>
-        <input type="submit" name="Enviar"/>
-    </form>
-    <?php
-    
+    <form action="<?php echo $_SERVER ['PHP_SELF'];?>" method="post">
+    <input type="hidden" name="intentos" value="
+        <?php 
+            if (!isset($_POST['intentos'])){
+            echo "0";
+            }else{
+                echo intval($_POST['intentos'])+1;
+            } 
+        ?>">
+    <label for="combinacion">Combinación</label>
+    <input type="number" name="combinacion"><br>
+    <input type="submit" name="Enviar">
+    <?php 
         $combinacionSecreta = "1111";
 
-        if(isset($_POST['combinacion']) && filter_var($_POST['combinacion'], FILTER_VALIDATE_REGEXP, array("options" => array("regexp" => "/[0-9]{4}/")))){
-            if($_POST['combinacion'] == $combinacionSecreta){
-                echo "<p>La caja fuerte se ha abierto satisfactoriamente</p>";
-            }else {
-                echo "<p>Lo siento. Esta NO es la combinación</p>";
+        if (isset($_POST['intentos']) && intval($_POST['intentos']< 4)) {
+            
+            if (isset($_POST['combinacion']) && filter_var($_POST['combinacion'], FILTER_VALIDATE_REGEXP, array("options" => array("regexp" => "/[0-9]{4}/")))) {
+                if ($_POST['combinacion']==$combinacionSecreta) {
+                    echo "<p>La caja fuerte se ha abierto satisfactoriamente</p>";
+                }else{
+                    echo "<p>Lo siento, esta no es la combinación</p>";
+                }
+            }else{
+                echo "<p>Introduce un número de catro díxitos.</p>";
             }
-
-        }else {
-            echo "<p>Introduce un número de cuatro dígitos</p>";
+        }elseif (isset($_POST['intentos'])) {
+            echo "<p>Ha superado el número de intentos</p>";
         }
 
     ?>
+    </form>
+
 </body>
 </html>
