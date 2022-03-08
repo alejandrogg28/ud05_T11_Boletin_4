9fc86a48602e27e0ea31b6fcebdc0dd26f97f633
Alejandro Garcia <alejandrogg28@gmail.com>
2022-03-08 19:37:33 +0100

    Ejercicio 11 Boletín 4 tercer commit

--- exercicio_11.html.php
+++ exercicio_11.html.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ehercicio 11 Boletín 4</title>
+    <title>Ejercicio 11 Boletín 4</title>
 </head>
 <body>
 <!--Escribe un programa que muestre en tres columnas, el cuadrado y el cubo de los 5 primeros números
@@ -22,18 +22,27 @@ enteros a partir de uno que se introduce por teclado.-->
         if(!isset($_POST['numero'])){
             echo "<p>Tienes que introducir un número</p>";
         }else if (isset($_POST['numero']) && filter_var($_POST['numero'],FILTER_VALIDATE_INT)) {
+            
+            echo "<p>Número introducido: ".$_POST['numero']."</p>";
             echo "<table border = 1>";
+            echo "<tr>"."<th>"."Cuadrado"."</th>"."<th>"."Cubo"."</th>"."</tr>";
+            
+            for ($i=0; $i <= 5; $i++) {
 
-            for ($i=0; $i <= 5; $i++) { 
-                echo "<tr>"."<th>".$_POST['numero']."Cuadrado".pow($_POST['numero'],2)."</th>"."</tr>";
-                echo "<tr>"."<th>".$_POST['numero']."Cubo".pow($_POST['numero'],3)."</th>"."</tr>";
+                echo "<tr>";
+                echo "<td>".$_POST['numero'].pow($_POST['numero'],2)."</td>";
+                echo "<td>".$_POST['numero'].pow($_POST['numero'],3)."</td>";
+                
+
                 $_POST['numero']++;
+                echo "</tr>";
             }
+            echo "</table>";
+
         }else {
             echo "<p>Tienes que introducir un número.</p>";
         }
-    
-    
+
     ?>
 
 </body>
